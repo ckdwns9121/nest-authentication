d0c104b70c1bd806c978ed4d3edca8277f2d1530
ckdwns9121 <ckdwns9121@naver.com>
2023-01-30 13:56:03 +0900

    # jwt-auth.guard 구현

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,15 +1,19 @@
 import {
   ExecutionContext,
+  HttpException,
+  HttpStatus,
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
 import { AuthGuard } from '@nestjs/passport';
 import { IS_PUBLIC_KEY } from '../auth.decorator';
+import { jwtConstants } from '../constants';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-  constructor(private reflector: Reflector) {
+  constructor(private reflector: Reflector, private jwtService: JwtService) {
     super();
   }
 
@@ -19,10 +23,42 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
       context.getHandler(),
       context.getClass(),
     ]);
+
+    const request = context.switchToHttp().getRequest();
+    const { authorization } = request.headers;
+    if (authorization === undefined) {
+      throw new HttpException('Token 전송 안됨', HttpStatus.UNAUTHORIZED);
+    }
+    const token = authorization.replace('Bearer ', '');
+    request.user = this.validateToken(token);
+
     if (isPublic) {
       return true;
     }
     return super.canActivate(context);
+  }
+
+  validateToken(token: string) {
+    const secretKey = jwtConstants.secret;
+    try {
+      const verify = this.jwtService.verify(token, { secret: secretKey });
+      return verify;
+    } catch (e) {
+      console.log(e);
+      switch (e.message) {
+        // 토큰에 대한 오류를 판단합니다.
+        case 'INVALID_TOKEN':
+        case 'TOKEN_IS_ARRAY':
+        case 'NO_USER':
+          throw new HttpException('유효하지 않은 토큰입니다.', 401);
+
+        case 'EXPIRED_TOKEN':
+          throw new HttpException('토큰이 만료되었습니다.', 410);
+
+        default:
+          throw new HttpException('서버 오류입니다.', 500);
+      }
+    }
   }
 
   handleRequest(err, user, info) {
