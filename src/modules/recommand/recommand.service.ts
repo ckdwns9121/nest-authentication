58ef5a7c82a3581b6737da8378605b5fd5505389
ChangjunPark <ckdwns9121@naver.com>
2023-01-31 21:11:54 +0900

    # 쿼리빌더에 엘리어스 적용

--- src/modules/recommand/recommand.service.ts
+++ src/modules/recommand/recommand.service.ts
@@ -1,8 +1,4 @@
-import {
-  Injectable,
-  NotFoundException,
-  BadRequestException,
-} from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Recommand } from './redommand.entity';
@@ -22,7 +18,6 @@ export class RecommandService {
     private userService: UserService,
   ) {}
 
-  // 추천인 추가하기
   async create(createRecommandDto: CreateRecommandDto): Promise<string> {
     const { operator, operands } = createRecommandDto;
 
@@ -32,13 +27,13 @@ export class RecommandService {
     }
 
     const existRecommand = await this.recommandRepository
-      .createQueryBuilder('recommand')
-      .innerJoinAndSelect('recommand.operator', 'user')
-      .where('user.id = :id', { id: operator })
+      .createQueryBuilder('r')
+      .innerJoinAndSelect('r.operator', 'u')
+      .where('u.id = :id', { id: operator })
       .getOne();
 
     if (existRecommand) {
-      throw new NotFoundException('이미 등록된 추천인이 있습니다.');
+      throw new NotFoundException('이미 등록한 추천인이 있습니다.');
     }
     return '성공';
   }
