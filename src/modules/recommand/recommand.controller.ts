e1bc885edc5baae4bd98516fef341864eba71db2
ChangjunPark <ckdwns9121@naver.com>
2023-02-01 15:47:15 +0900

    # 추천인 등록 및 나를 추천인으로 등록하는 사람 가져오기

--- src/modules/recommand/recommand.controller.ts
+++ src/modules/recommand/recommand.controller.ts
@@ -15,12 +15,23 @@ export class RecommandController {
   constructor(private recommandService: RecommandService) {}
 
   @Get()
-  findAllMy() {
-    return 'this will return recommand';
+  findAllMy(@Req() req) {
+    return this.recommandService.findAll(req.user.id);
   }
 
   @Post()
-  async create(@Body(ValidationPipe) createRecommandDto: CreateRecommandDto) {
+  async create(
+    @Body(ValidationPipe) createRecommandDto: CreateRecommandDto,
+    @Req() req,
+  ) {
+    if (req.user.id !== createRecommandDto.operator) {
+      throw new BadRequestException('잘못된 요청입니다.');
+    }
+    if (req.user.id === createRecommandDto.operands) {
+      throw new BadRequestException(
+        '자기 자신을 추천인으로 등록할 수 없습니다..',
+      );
+    }
     return this.recommandService.create(createRecommandDto);
   }
 }
--- src/modules/recommand/recommand.service.ts
+++ src/modules/recommand/recommand.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { In, Repository } from 'typeorm';
 import { Recommand } from './redommand.entity';
 import { CreateRecommandDto } from './dto/create-recommand.dto';
 import { UserService } from 'src/modules/user/user.service';
@@ -18,23 +18,61 @@ export class RecommandService {
     private userService: UserService,
   ) {}
 
+  async findAll(id: string): Promise<any> {
+    const user: User = await this.userService.findOneById(id);
+    const { operands } = user;
+    const recommandIds = operands.map((item) => item.recommand_id);
+
+    // const recommanders = await this.recommandRepository.find({
+    //   where: { recommand_id: In(recommandIds) },
+    // });
+
+    const recommanders = await this.userRepository.find({
+      where: {
+        operator: {
+          recommand_id: In([recommandIds]),
+        },
+      },
+    });
+    return recommanders.map((item) => {
+      return {
+        id: item.id,
+        create_at: item.created_at,
+      };
+    });
+  }
+
   async create(createRecommandDto: CreateRecommandDto): Promise<string> {
     const { operator, operands } = createRecommandDto;
 
-    const existUser = await this.userService.findOneById(operands);
-    if (!existUser) {
+    const existOperandUser: User = await this.userService.findOneById(operands);
+    if (!existOperandUser) {
       throw new NotFoundException('존재하지 않는 추천인 아이디입니다.');
     }
 
-    const existRecommand = await this.recommandRepository
-      .createQueryBuilder('r')
-      .innerJoinAndSelect('r.operator', 'u')
-      .where('u.id = :id', { id: operator })
-      .getOne();
+    // const existRecommand = await this.recommandRepository
+    //   .createQueryBuilder('r')
+    //   .innerJoinAndSelect('r.operator', 'u')
+    //   .where('u.id = :id', { id: operator })
+    //   .getOne();
+
+    const existRecommand = await this.recommandRepository.findOne({
+      where: { operator: { id: operator } },
+    });
 
     if (existRecommand) {
       throw new NotFoundException('이미 등록한 추천인이 있습니다.');
     }
-    return '성공';
+
+    const operatorUser: User = await this.userService.findOneById(operator);
+
+    const data = await this.recommandRepository.create({
+      operator: operatorUser,
+      operands: existOperandUser,
+    });
+
+    await this.recommandRepository.save(data);
+
+    return `${operator}가 ${operands}를 추천인으로 등록했습니다.`;
   }
 }
