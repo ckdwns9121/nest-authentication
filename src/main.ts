5a9381809575318d948092a81d870ac5ca51de81
changjun <ckdwns9121@naver.com>
2023-02-01 23:14:51 +0900

    # 스웨거 적용

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { setupSwagger } from './providers/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  setupSwagger(app);
   await app.listen(3000);
 }
 bootstrap();
